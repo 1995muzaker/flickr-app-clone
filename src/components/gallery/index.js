ef9296be0e49e79ebbfa84cb7142be4b9979bf3a
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2020-04-28 03:10:50 +0530

    gallery layout in masonry integrated

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,28 +1,6 @@
-// import React from "react";
-
-// class Gallery extends React.Component {
-//   state = {
-//     id: null,
-//   };
-//   componentDidMount() {
-//     let id = this.props.match.params.post_id;
-//     this.setState({
-//       id,
-//     });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <p>{this.state.id}</p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Gallery;
-
 import React, { Component } from "react";
 import axios from "axios";
+import { MansoryDiv } from "../../styles/Gallery";
 
 class Gallery extends Component {
   constructor() {
@@ -30,18 +8,10 @@ class Gallery extends Component {
     this.state = {
       pictures: [],
     };
-    // this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     this.loadImg();
   }
-//   Delay = (function () {
-//     var timer = 0;
-//     return function (callback, ms) {
-//       clearTimeout(timer);
-//       timer = setTimeout(callback, ms);
-//     };
-//   })();
 
   loadImg = () => {
     let id = this.props.match.params.post_id;
@@ -59,77 +29,53 @@ class Gallery extends Component {
         }
       )
       .then((res) => {
+        console.log(res);
         this.setState({
           pictures: res.data.photos.photo,
-        })
+        });
       });
-
-    // let str = (this.state.searchString).length > 0 ? (this.state.searchString): 'birds';
-    // console.log(str);
-
-    // fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=0cfe5982584801c4b835e6231df36efb&tags='+str+'&per_page='+(this.state.numPhotos)+'&page=1&safe_search=1&format=json&nojsoncallback=1')
-    // .then(function(response){
-    //     return response.json();
-    // })
-    // .then(function(j){
-    //     let picArray = j.photos.photo.map((pic) => {
-    //     console.log(pic);
-    //     var titlePic = pic.title;
-    //     var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
-    //     var KeyID = pic.id;
-    //     var KeyID2 =srcPath;
-    //     return(
-    //         <div>
-    //             <h3 key={KeyID} >{titlePic}</h3>
-    //             <img key={KeyID2} alt="birds" src={srcPath}></img>
-    //         </div>
-
-    //     )
-    //     })
-    //     this.setState({pictures: picArray});
-    // }.bind(this))
   };
-
-  // here we are setting up the code for string search that user types in the input field
-  //   handleChange(event) {
-  //     const { name, value, type } = event.target;
-  //     if (type === "select-one") {
-  //       let newNumPhotos = value;
-  //       this.setState({ numPhotos: newNumPhotos });
-  //     }
-  //     if (type === "text") {
-  //       let newSearchString = value;
-  //       this.setState({ searchString: newSearchString });
-  //     }
-  //     this.loadImg();
-  //   }
 
   render() {
     return (
-      <div>
+      <MansoryDiv>
         {this.state.pictures.map((grpData) => {
           return (
-            <div key={grpData.id}>
-              <div>
-                <img
-                  src={
-                    "https://farm" +
-                    grpData.farm +
-                    ".staticflickr.com/" +
-                    grpData.server +
-                    "/" +
-                    grpData.id +
-                    "_" +
-                    grpData.secret +
-                    ".jpg"
-                  }
-                  alt="img"
-                />
+            <div className="grid" key={grpData.id}>
+              <img
+                src={
+                  "https://farm" +
+                  grpData.farm +
+                  ".staticflickr.com/" +
+                  grpData.server +
+                  "/" +
+                  grpData.id +
+                  "_" +
+                  grpData.secret +
+                  ".jpg"
+                }
+                alt="img"
+              />
+              <div className="grid__body">
+                <div className="parent-inner">
+                  <div className="inner-div">
+                    <div className="relative">
+                      <p className="grid__title">{grpData.title}</p>
+                      <div className="author-div">
+                        <span>Author:</span>&nbsp;
+                        <p className="grid__author">{grpData.ownername}</p>
+                      </div>
+                    </div>
+                    {/* <div className="update">
+                      <p>posted at: {grpData.dateadded}</p>
+                    </div> */}
+                  </div>
+                </div>
               </div>
             </div>
           );
         })}
-      </div>
+      </MansoryDiv>
     );
   }
 }
---
+++ src/styles/Gallery.js
@@ -0,0 +1,113 @@
+import styled from "styled-components";
+
+export const MansoryDiv = styled.div`
+  columns: 4;
+  column-gap: 16px;
+  padding: 50px;
+  @media (max-width: 1200px) {
+    columns: 3;
+  }
+  @media (max-width: 992px) {
+    columns: 2;
+  }
+  .grid {
+    display: inline-block;
+    margin-bottom: 16px;
+    position: relative;
+    &:before {
+      border-radius: 5px;
+      content: "";
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+    &__title {
+      font-size: 18px;
+      font-weight: 500;
+      margin: 0px 0px;
+      text-align: left;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-decoration: underline;
+      margin-bottom: 5px;
+    }
+    &__author {
+      text-transform: capitalize;
+      font-size: 12px;
+      font-weight: 500;
+      margin: 0;
+      text-align: left;
+      display: -webkit-box;
+      -webkit-line-clamp: 1;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+    &__link {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+    }
+    &__body {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      padding: 10px;
+      color: #fff;
+      display: flex;
+      flex-direction: column;
+    }
+    &__tag {
+      background-color: rgba(0, 0, 0, 0.35);
+      color: #333;
+      border-radius: 5px;
+      padding: 5px 15px;
+      margin-bottom: 5px;
+    }
+    & .parent-inner {
+      display: flex;
+      justify-content: space-between;
+      align-items: flex-end;
+      height: 100%;
+      & .inner-div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: auto;
+        background: #000000a3;
+        border-radius: 3px;
+        width: 100%;
+        padding: 10px;
+      }
+      & .author-div {
+        display: flex;
+        align-items: center;
+        & span{
+            text-transform: capitalize;
+      font-size: 12px;
+      font-weight: 500;
+      margin: 0;
+      text-align: left;
+        }
+      }
+      & .update p {
+        font-size: 12px;
+        font-weight: 500;
+        margin: 0;
+        text-align: right;
+      }
+    }
+  }
+`;
